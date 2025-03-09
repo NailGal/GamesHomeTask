664a8d2b6ae1fb3c43b46aaded0a4dad945858a8
NailGal <galiyllin@gmail.com>
2025-03-09 12:24:30 +0400

    Edit error1 from teacher

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,107 @@ import java.io.IOException;
 public class Main {
     public static final String rootPath = "C://Users//Admin//Games/";
     public static File temp;
+    public static File file;
 
     public static void main(String[] args) {
-        createDirectoryTemp();
-        createDirectorySrc();
-        createDirectoryRes();
-        createDirectorySaveGames();
-
-
+//        createDirectoryTemp();
+//        createDirectorySrc();
+//        createDirectoryRes();
+//        createDirectorySaveGames();
+        // Создание папки temp
+        createDirectoryTemp(rootPath + "temp/");
+        createFileTemp(rootPath + "temp/", "temp.txt");
+        createDirectory(rootPath + "src/");
+        createDirectory(rootPath + "src/");
+        createDirectory(rootPath + "src/main/");
+        createDirectory(rootPath + "src/test/");
+        createFile(rootPath + "src/main/", "main.java");
+        createFile(rootPath + "src/main/", "Utils.java");
+        createDirectory(rootPath + "res/");
+        createDirectory(rootPath + "res/drawables");
+        createDirectory(rootPath + "res/vectors");
+        createDirectory(rootPath + "res/icons");
+        createDirectory(rootPath + "savegames/");
     }
 
+    public static void createDirectoryTemp(String path) {
+        StringBuilder sbDir = new StringBuilder(); // Создаем StringBuilder внутри метода в момент создания файла*
+        File createDir = new File(path);
+        if (createDir.mkdirs()) {
+            sbDir.append("Создан каталог " + createDir.getName() + ", путь: " + createDir.getAbsolutePath()+ "\n");
+            System.out.println("Каталог " + createDir.getName()+ " создан");
+        }
+    }
+
+    public static void createFileTemp(String pathFile, String nameFile){
+        StringBuilder sbFile = new StringBuilder();
+        try {
+            temp = new File(pathFile, nameFile);
+            if (temp.createNewFile()) {
+                sbFile.append("Создан файл " + temp.getName() + " " + temp.getAbsolutePath()+ "\n");
+                System.out.println("Файл temp.txt создался");
+            }
+        } catch (IOException e) {
+            System.out.println("Файл temp.txt не создался");
+        }
+    }
+    // Один общий метод для создания папок (Директорий)
+    public static void createDirectory(String path) {
+        StringBuilder sbDir = new StringBuilder(); // Создаем StringBuilder внутри метода в момент создания файла*
+        File createDir = new File(path);
+        if (createDir.mkdirs()) {
+            sbDir.append("Создан каталог " + createDir.getName() + ", путь: " + createDir.getAbsolutePath()+ "\n");
+            writeToFileTempDir(sbDir);
+            System.out.println("Каталог " + createDir.getName()+ " создан");
+        }
+    }
+
+    // Метод для создания файлов
+    public static void createFile(String pathFile, String nameFile){
+        StringBuilder sbFile = new StringBuilder();
+        try {
+            file = new File(pathFile, nameFile);
+            if (file.createNewFile()) {
+                sbFile.append("Создан файл " + file.getName() + " " + file.getAbsolutePath()+ "\n");
+                writeToFileTempFile(sbFile);
+                System.out.println("Файл "+ file.getName() + " создался");
+            }
+        } catch (IOException e) {
+            System.out.println("Файл temp.txt не создался");
+        }
+    }
+
+    public static void writeToFileTempDir(StringBuilder sb) {
+        try (FileOutputStream fos = new FileOutputStream(temp, true)) {
+            String text = sb.toString();
+            byte[] buffer = text.getBytes();
+            fos.write(buffer);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void writeToFileTempFile(StringBuilder sb) {
+        try (FileOutputStream fos = new FileOutputStream(temp, true)) {
+            String text = sb.toString();
+            byte[] buffer = text.getBytes();
+            fos.write(buffer);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /*
     public static void createDirectoryTemp() {
-        String tempPath = rootPath + "temp/";
+        String tempPath = rootPath + "temp/"; // tempPath = C://Users//Admin//Games/temp/
         File createdTemp = new File(tempPath);
         if (createdTemp.mkdirs()) {
             System.out.println("Каталог temp создан");
         }
 
         try {
-            temp = new File(createdTemp, "temp.txt");
-            if (temp.createNewFile()) {
+            file = new File(createdTemp, "temp.txt");  // temp - это объект типа File, созданный в самом начале
+            if (file.createNewFile()) { // Команда для создания файла, находится в условии проверки
                 System.out.println("Файл temp.txt создался");
                 writeToFileTemp(createdTemp);
             }
@@ -34,9 +115,10 @@ public class Main {
         }
 
     }
+    */
 
     public static void writeToFileTemp(File file) {
-        try (FileOutputStream fos = new FileOutputStream(temp, true)) {
+        try (FileOutputStream fos = new FileOutputStream(file, true)) {
             StringBuilder sb = new StringBuilder();
             sb.append("Создан каталог " + file.getAbsolutePath()+ "\n");
             String text = sb.toString();
@@ -47,8 +129,9 @@ public class Main {
         }
     }
 
+    /*
     public static void writeToFileTempFile(File file) {
-        try (FileOutputStream fos = new FileOutputStream(temp, true)) {
+        try (FileOutputStream fos = new FileOutputStream(file, true)) {
             StringBuilder sb = new StringBuilder();
             sb.append("Создан файл " + file.getName()+ " в каталоге "+ file.getAbsolutePath()+"\n");
             String text = sb.toString();
@@ -58,8 +141,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
-
-
+     */
 
     public static void createDirectoryRes() {
         String resPath = rootPath + "res/";
@@ -117,7 +199,7 @@ public class Main {
             }
         }
     }
-
+/*
     public static void createDirectorySrc() {
         String srcPath = rootPath + "src/";
         File srcPathDir = new File(srcPath);
@@ -172,5 +254,5 @@ public class Main {
             }
 
         }
-    }
+    }*/
 }
